fe91af0bb7bbc1ea0cfaaf7d0f9e5c6941d938ea
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2023-08-28 19:13:56 +0530

    added config module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const config = require("config");
 
 const connectDB = require("./config/db");
 connectDB();
